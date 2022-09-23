8d39cfdd64f0425064e7c46946739d38d25efd43
qkrdmstlr3 <qkrdmstlr3@naver.com>
2022-09-23 21:37:33 +0900

    ::

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import "./App.css";
+
+const A = styled.a``;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <A
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </A>
       </header>
     </div>
   );
